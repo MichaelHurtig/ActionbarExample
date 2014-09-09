16ab93599a4b4a2b90d6d60d3e1d4338b4ac601b
Michael Hurtig <michael.v.hurtig@gmail.com>
2014-09-09 14:54:33 -0600

    Actionbar Working!
    I have the action bar working, now.  This is the last I am going to work
    on this project.

--- app/src/main/java/michaelhurtig/actionbarexample/ActionbarExample.java
+++ app/src/main/java/michaelhurtig/actionbarexample/ActionbarExample.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ActionbarExample extends ActionBarActivity {
@@ -31,17 +32,30 @@ public class ActionbarExample extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /** Called when the user clicks the Send button */
+
+    private void openSearch() {
+        Toast.makeText(this, "Search button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSettings() {
+        Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
